30bacbb1c97f611c6fb1138da80bf4dd0c4f69f2
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 16:19:09 +0300

    added update profile form

--- awwards/views.py
+++ awwards/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from . models import Profile
+from .forms  import ProfileModelForm
+
 
 # Create your views here.
 def my_profile_view(request):
     profile = Profile.objects.get(user = request.user)
-            
+    form = ProfileModelForm(request.POST or None ,request.FILES or None,instance = profile)
+    comfirm = False
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            comfirm = True
+        form = ProfileModelForm()   
+         
     context = {
         'profile':profile,
-      
+        'form':form,
+        'comfirm':comfirm,
+
     }
     return render(request,'profiles/myprofile.html',context)
 
