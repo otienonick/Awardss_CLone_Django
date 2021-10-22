9398838b4e56459a968f02b4e04d6b2adb7df4af
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 21:44:34 +0300

    added reviewphoto view function

--- awwards/views.py
+++ awwards/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . models import Profile,Post
-from .forms  import ProfileModelForm
+from .forms  import ProfileModelForm,PostModelForm
 
 
 # Create your views here.
@@ -8,17 +8,29 @@ def my_profile_view(request):
     profile = Profile.objects.get(user = request.user)
     posts = Post.objects.filter(author=request.user).order_by('-created')
     form = ProfileModelForm(request.POST or None ,request.FILES or None,instance = profile)
+    p_form = PostModelForm(request.POST or None,request.FILES or None)
+
     comfirm = False
     if request.method == 'POST':
         if form.is_valid():
             form.save()
             comfirm = True
+
+            
+    if p_form.is_valid():
+        instance = p_form.save(commit=False)
+        instance.author = request.user
+        instance.save()
+        return redirect('home')
+    p_form = PostModelForm()            
          
     context = {
         'profile':profile,
         'form':form,
         'comfirm':comfirm,
-        'posts':posts
+        'posts':posts,
+        'p_form':p_form,
+
 
     }
     return render(request,'profiles/myprofile.html',context)
@@ -32,3 +44,13 @@ def home(request):
     }
 
     return render(request,'awwards/home.html',context)
+
+def reviewPhoto(request,pk):
+    post = Post.objects.get(id = pk)
+    context = {
+        'post':post
+
+    }
+
+
+    return render(request,'awwards/review.html',context)
