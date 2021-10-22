cb2f7f3510ab64268cd3bf5dafec53cc965dcae1
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 16:17:17 +0300

    updated image field to show default image

--- awwards/models.py
+++ awwards/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 # Create your models here.
 class Profile(models.Model):
     username = models.CharField(max_length = 255,unique=True)
