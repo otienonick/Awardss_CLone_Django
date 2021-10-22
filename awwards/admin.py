fe4befe70a41232cd1cae99831c9151cb11301b5
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 22:52:23 +0300

    made migrations and also included votes in the admin page

--- awwards/admin.py
+++ awwards/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Profile,Post
+from .models import Profile,Post,Like
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(Like)
+
 
 
