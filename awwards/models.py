b5844cab0a9152ceac7428950d6eddc12eb9eb7a
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 22:51:20 +0300

    created vote model

--- awwards/models.py
+++ awwards/models.py
@@ -32,3 +32,18 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created'] 
+
+LIKE_CHOICES =(
+    ('Like','Like'),
+    ('Unlike','Unlike')
+)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(Profile,on_delete=models.CASCADE)     
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)  
+    value = models.CharField(choices=LIKE_CHOICES,max_length=8)   
+
+    def __str__(self):
+        return f'{self.user}-{self.post}-{self.value}'
+
