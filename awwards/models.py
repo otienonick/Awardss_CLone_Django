18c5094336452d736ab2448a2720f03f8a03a6ed
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 14:38:02 +0300

    created profile class  model

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length = 255,unique=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    bio = models.TextField(default='no bio...',max_length=300)
+    avatar = models.ImageField(default='avatar.png',upload_to = 'avatars/')
+    created = models.DateTimeField(auto_now_add=True)
