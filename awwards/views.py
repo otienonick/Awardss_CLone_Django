776409451a89825fa83bb1a85680411af753644c
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 22:50:47 +0300

    added vote review view function

--- awwards/views.py
+++ awwards/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from . models import Profile,Post
+from . models import Profile,Post,Like
 from .forms  import ProfileModelForm,PostModelForm
 
 
@@ -54,3 +54,28 @@ def reviewPhoto(request,pk):
 
 
     return render(request,'awwards/review.html',context)
+
+def like(request):
+    user = request.user
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        post_obj = Post.objects.get(id = post_id)
+        profile = Profile.objects.get(user = user)
+        if profile in post_obj.liked.all():
+            post_obj.liked.remove(profile)
+        else:
+            post_obj.liked.add(profile)
+        like,created = Like.objects.get_or_create(user = profile,post_id = post_id)
+        if not created:
+            if like.value == 'Like':
+                like.value='Unlike'
+            else:
+                like.value ='Like'   
+        else:
+            like.value = 'Like'     
+
+            post_obj.save()
+            like.save()
+            
+      
+    return redirect('review',pk = post_id )
