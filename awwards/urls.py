a4b1fa227e3f9f02ea0a006bf2fff1d06cb166b2
otienonick <nick.odongi@student.moringaschool.com>
2021-10-22 14:32:55 +0300

    created awwards urls module

---
+++ awwards/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import my_profile_view
+
+urlpatterns = [
+    path('myprofile/',my_profile_view,name ='profile'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
