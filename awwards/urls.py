3428343ad19290c1743fcc3927f975c766d0cd2f
otienonick <nick.odongi@student.moringaschool.com>
2021-10-23 11:36:05 +0300

    added search urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path 
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import my_profile_view,home,reviewPhoto,like
+from .views import my_profile_view,home,reviewPhoto,like,search_results
 
 urlpatterns = [
     path('myprofile/',my_profile_view,name ='profile'),
     path('' , home , name='home'),
     path('reviewPost/<str:pk>' , reviewPhoto , name='review'),
     path('like/',like, name = 'like'),
+    path('search/',search_results, name ='search_results'),
+
 
 
 ]
