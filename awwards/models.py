e7ae0a686e8f67d1e26a491bd55fe3bd4801b60e
otienonick <nick.odongi@student.moringaschool.com>
2021-10-23 10:16:11 +0300

    modified like model to display total number of likes

--- awwards/models.py
+++ awwards/models.py
@@ -43,6 +43,7 @@ class Like(models.Model):
     user = models.ForeignKey(Profile,on_delete=models.CASCADE)     
     post = models.ForeignKey(Post,on_delete=models.CASCADE)  
     value = models.CharField(choices=LIKE_CHOICES,max_length=8)   
+  
 
     def __str__(self):
         return f'{self.user}-{self.post}-{self.value}'
